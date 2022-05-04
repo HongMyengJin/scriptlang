3006f3fb0f85a9d7634e12ad62a311377d87b8f4
홍명진 <mongjinjin@naver.com>
2022-05-05 02:01:05 +0900

    list Bug Fixed

--- Games/Tic-Tac-Toe.py
+++ Games/Tic-Tac-Toe.py
@@ -11,12 +11,15 @@ class cell(Label):
     global cells
     def __init__(self, num):
         self.token = ''
-        self.num = num
-        imgLabel = Label(root, image = img[0])
+        self.num = int(num)
+        self.imgLabel = Label(root, image = img[0])
 
+    @property
+    def GetToken(self):
+        return self.token
     def Win(self):
-        row = self.num / 3
-        column = self.num % 3
+        row = (self.num) // 3
+        column = (self.num) % 3
 
         # 갯수를 세준다.
         # 첫번째꺼 저장 뒤에 2개 같으면
@@ -26,7 +29,7 @@ class cell(Label):
             for i in range(0, 3):
                 if j == 0:
                     if cells[row][column].token != cells[i][column].token:
-                        break
+                        break 
                 else:
                        if cells[row][column].token != cells[row][i].token:
                         break
@@ -51,15 +54,17 @@ class cell(Label):
             return False # 이미 뒤집은 패
         self.token = event
         if    self.token == 'O':
-            imgLabel = Label(root, image = img[1])
+            self.imgLabel = Label(root, image = img[1])
         elif  self.token == 'X':
-            imgLabel = Label(root, image = img[2])
+            self.imgLabel = Label(root, image = img[2])
         
-        return  cell.Win()# 새로 등록된 패 #혹시 모르니까 오류뜨면 여기일듯
+        root.update()
+        return  cell.Win(self)# 새로 등록된 패 #혹시 모르니까 오류뜨면 여기일듯
         
     
+    def Render(self):
+        self.imgLabel.grid(row= (int)(self.num / 3), column= (int)(self.num % 3))
 
-    
 
 
         
@@ -68,59 +73,29 @@ class cell(Label):
         #대각선 => 없으면 False
 
 root = Tk()
-root.title('tec-tac-toe')      
+root.title('tic-tac-toe')      
 img = [PhotoImage(file = "empty.gif"), PhotoImage(file = "o.gif"), PhotoImage(file = "x.gif")]
 
-cells = [cell(i) for i in range(1,10)]
+#cells = [cell(i) for i in range(0,9)]
 
+cells = [[cell(i * 3 + j) for i in range(0, 3)] for j in range(0, 3)]
 
-
-#frameTitle = Frame(root, padx=120, pady=120)
-
-#frameTitle.grid(row=2, column=2) #, sticky='w'
-#Label(frameTitle).grid(row=0, column=0)
-
-
+for i in range(0, 3):
+    for j in range(0, 3):
+        cells[i][j].Render()
 
 imgLabel = [Label(root, image = img[0]) for i in range(1, 10)]
 
 
-        
-# imgLabel1 = Label(root, image = img[1])
-# imgLabel2 = Label(root, image = img[1])
-# imgLabel3 = Label(root, image = img[1])
-# imgLabel1.grid(row = 0, column = 0)
-# imgLabel2.grid(row = 0, column = 1)
-# imgLabel3.grid(row = 0, column = 2)
-
-imgLabel[0] = Label(root, image = img[0])
-t = 0
-for j in range (0, 3):
-    for i in range(0, 3):
-        imgLabel[t].grid(row= i, column=j)
-        t = t + 1
-
-# frameCombo = Frame(root, padx=10, pady=10)
-# frameCombo.grid(row=1, column=0, sticky='news') 
-# combo_str = ['수퍼수프림', '감자피자', '고구마피자'] 
-# combo = ttk.Combobox(frameCombo, values=combo_str) 
-# combo.pack(side=LEFT, expand=True, fill='both') 
-# combo.set('피자 선택')
-
-# frameCheckbox = Frame(root, padx=10, pady=10)
-# frameCheckbox.grid(row=2, column=0) 
-# chkValue = [] 
-# strCheck = ['치즈 추가', '피클 추가', '콜라 추가']
-# for i, s in enumerate(strCheck): chkValue.append(IntVar()) # Variable 클래스: get(), set() 함수 사용
-# ttk.Checkbutton(frameCheckbox, text=s, variable=chkValue[i]).grid(row = 0, column=i)
-# frameButtons = Frame(root, padx=10, pady=10, bg='#a0a0a0')
-# frameButtons.grid(row=3, column=0, sticky='news')
-# Button(frameButtons, command=onOK, text='확인').pack(side=LEFT, expand=True, fill='both')
-# Button(frameButtons, command=onCancel, text='취소').pack(side=RIGHT, expand=True, fill='both') 
-
 frameTitle = Frame(root, padx=120, pady=120)
 Label(frameTitle).grid(row=0, column=0)
 
+a = input("행과 열을 입력하세요.").split()
 
+# #행
+# a[0] 
+# #열
+# a[1]
 
+cells[0][0].flip('O')
 root.mainloop()
