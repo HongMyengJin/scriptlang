3bbe414719ab68f52a05ff59ceabcb381a4e4c8c
이재윤 <ljy9356@gmail.com>
2022-05-04 22:41:39 +0900

    add image

--- Games/tec-tac-toe.py
+++ Games/tec-tac-toe.py
@@ -7,35 +7,11 @@ currentToken = 0
 class cell(Label):
 
     def __init__(self):
-        self.flipvalue = False
+        self.token = ''
 
-    def test(self):
-        self.flipvalue = True
+    # def flip(self, event):
 
-    def FlipValueReturn(self):
-        return self.flipvalue
-
-def onOK(): # “확인”버튼핸들러
-    global chkValue, strCheck
-    for i, v in enumerate(chkValue):
-        if v.get() == 1: # Variable 클래스: get()함수 사용
-            print(strCheck[i], "선택됨") 
-    messagebox.showinfo(title='알림', message='피자가 선택되었습니다')
-    root.destroy()
-
-def onCancel(): # “취소”버튼핸들러
-    messagebox.showinfo(title='알림', message='피자 선택이 취소되었습니다')
-    root.destroy()
-
-
-Cells = [cell() for i in range(1,10)]
-
-for i in range(0, 5):
-    Cells[i].test()
-
-for i in range(0, 9):
-    print(Cells[i].FlipValueReturn())
-
+cells = [cell() for i in range(1,10)]
 
 
 
@@ -43,10 +19,30 @@ for i in range(0, 9):
 root = Tk()
 root.title('tec-tac-toe')
 
-frameTitle = Frame(root, padx=120, pady=120)
+#frameTitle = Frame(root, padx=120, pady=120)
 
-frameTitle.grid(row=0, column=0, sticky='w')
-Label(frameTitle).grid(row=0, column=0)
+#frameTitle.grid(row=2, column=2) #, sticky='w'
+#Label(frameTitle).grid(row=0, column=0)
+
+img = [PhotoImage(file = "o.gif"), PhotoImage(file = "x.gif"), PhotoImage(file = "empty.gif")]
+
+
+imgLabel = [Label(root, image = img[2]) for i in range(1, 10)]
+
+
+        
+# imgLabel1 = Label(root, image = img[1])
+# imgLabel2 = Label(root, image = img[1])
+# imgLabel3 = Label(root, image = img[1])
+# imgLabel1.grid(row = 0, column = 0)
+# imgLabel2.grid(row = 0, column = 1)
+# imgLabel3.grid(row = 0, column = 2)
+
+t = 0
+for j in range (0, 3):
+    for i in range(0, 3):
+        imgLabel[t].grid(row= i, column=j)
+        t = t + 1
 
 # frameCombo = Frame(root, padx=10, pady=10)
 # frameCombo.grid(row=1, column=0, sticky='news') 
