f53ce729a98d5061f9dd7164a110f1d9826f87ee
홍명진 <mongjinjin@naver.com>
2022-05-08 22:30:27 +0900

    End

--- Games/Tic-Tac-Toe.py
+++ Games/Tic-Tac-Toe.py
@@ -22,10 +22,8 @@ class cell(Label):
         if  cells[row][column].token != '' or Quit:
             return False # 이미 뒤집은 패`
         if  currentToken % 2 == 0:
-            #self.token = 'O'
             cells[row][column].token = 'O'
         else:
-            #self.token = 'X'
             cells[row][column].token = 'X'
 
         if  cells[row][column].token == 'O':
@@ -59,7 +57,6 @@ class cell(Label):
         Label(root, text=texts, width = 25).place(x = -30, y = 130) 
 
         return False
-        #QLabels.text = texts
         
     def __init__(self, num):
         self.token = ''
@@ -69,7 +66,7 @@ class cell(Label):
         super().__init__(root, padx=14, pady=7.2)
         super().bind("<Button-1>", self.flip)
         super().place(width=14,height=7.2)
-
+        self.Render()
     def GetToken(self):
         return self.token
     
@@ -79,14 +76,11 @@ class cell(Label):
         else:
             return False
                     
-
     def Win(self):
         row = (self.num) // 3
         column = (self.num) % 3
-
         # 갯수를 세준다.
         # 첫번째꺼 저장 뒤에 2개 같으면
-
         # 행, 열
         for i in range(0, 3):
             if  cells[row][column].token != cells[i][column].token:
@@ -98,8 +92,6 @@ class cell(Label):
                 break 
         else:
             return True # 3개 이상
-
-
         # 대각선
         if  (row == column): # 대각선 검사를 할지 결정   # 행, 열이 같다    
             for i in range(1, 3):
@@ -110,59 +102,19 @@ class cell(Label):
         elif (abs(row - column) == 2): #행과 열의 차 = 2     
             if (cells[0][2].token == cells[1][1].token == cells[2][0].token):
                 return True
-
-
         return False
     
     def Render(self):
         self.imgLabel.grid(row= (int)(self.num / 3), column= (int)(self.num % 3))
         super().grid(row = self.num // 3, column = self.num % 3)
-
-
-
-        
         #해당 행, 열만 체크 => 있으면 True
          
         #대각선 => 없으면 False
 
 root = Tk()
-
 root.geometry('120x150')
 root.title('tic-tac-toe')      
 img = [PhotoImage(file = "empty.gif"), PhotoImage(file = "o.gif"), PhotoImage(file = "x.gif")]
-
-
-#cells = [cell(i) for i in range(0,9)]
-
 cells = [[cell(i * 3 + j) for i in range(0, 3)] for j in range(0, 3)]
-
-
-for i in range(0, 3):
-    for j in range(0, 3):
-        cells[i][j].Render()
-
 imgLabel = [Label(root, image = img[0]) for i in range(1, 10)]
-
-
-
-
-
-# for i in range(0, 3):
-#      for j in range(0, 3):
-#         cells[i][j].bind("<Button-1>") 
-
-# a = input("행과 열을 입력하세요.").split()
-
-# # #행
-# # a[0] 
-# # #열
-# # a[1]
-
-#cells[0][0].flip('O')
-
-
- 
-
- #왼쪽 마우스 버튼 바인딩
-
 root.mainloop()
