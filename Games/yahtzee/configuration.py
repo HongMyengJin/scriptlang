be9f14130c29c47a1d51379e0975e8ee8dc60fee
HongMyengJin <84975902+HongMyengJin@users.noreply.github.com>
2022-06-06 13:49:09 +0900

    configuration Modify

--- Games/yahtzee/configuration.py
+++ Games/yahtzee/configuration.py
@@ -20,5 +20,75 @@ class Configuration:
     @staticmethod
     def score(row, dices):       # 정적 메소드 (객체 없이 사용 가능)
         # TODO: 구현
-        return 0
+        dicelist = []
+        for n in dices:
+            dicelist.append(n.getRoll())
+        if (row >= 0 and row <= 6):
+            return Configuration.scoreUpper(dicelist, row + 1)
+        elif (row == 8):
+            return Configuration.scoreThreeOfAKind(dicelist)
+        elif (row == 9):
+            return Configuration.scoreFourOfAKind(dicelist)
+        elif (row == 10):
+            return Configuration.scoreFullHouse(dicelist)
+        elif (row == 11):
+            return Configuration.scoreSmallStraight(dicelist)
+        elif (row == 12):
+            return Configuration.scoreLargeStraight(dicelist)
+        elif (row == 13):
+            return Configuration.scoreYahtzee(dicelist)
+        elif (row == 14):
+            return Configuration.sumDie(dicelist)
+
+    def scoreUpper(d, num):  # 정적 메소드: 객체생성 없이 사용 가능
+
+        # Upper Section 구성 (Ones, Twos, Threes, ...)에 대해 주사위 점수를 매 깁니다. 예를 들어,
+        # num이 1이면 "Ones"구성의 주사위 점수를 반환합니다.
+
+        return d.count(num)*num
+
+    def scoreThreeOfAKind(d):
+        for i in range(len(d)+1):
+            if d.count(i+1) >= 3:
+                return sum(d)
+        return False
+
+    def scoreFourOfAKind(d):
+        for i in range(len(d)+1):
+            if d.count(i+1) >= 4:
+                return sum(d)
+        return False
+
+    def scoreFullHouse(d):
+        for i in range(len(d)+1):
+            for j in range(len(d)):
+                if d.count(i+1) is 2 and d.count(j+1) is 3:
+                    return 25
+                if d.count(i+1) is 3 and d.count(j+1) is 2:
+                    return 25
+        return False
+
+    def scoreSmallStraight(d):
+        # 1 2 3 4 혹은 2 3 4 5 혹은 3 4 5 6 검사
+        # 1 2 2 3 4, 1 2 3 4 6, 1 3 4 5 6, 2 3 4 4 5
+        if (1 in d and 2 in d and 3 in d and 4 in d) or (2 in d and 3 in d and 4 in d and 5 in d) or (3 in d and 4 in d and 5 in d and 6 in d):
+            return 30
+        return False
+
+    def scoreLargeStraight(d):
+
+        # 1 2 3 4 5 혹은 2 3 4 5 6 검사
+        if (1 in d and 2 in d and 3 in d and 4 in d and 5 in d) or (2 in d and 3 in d and 4 in d and 5 in d and 6 in d) :
+            return 40
+        return False
+
+    def scoreYahtzee(d):
+        for i in range(len(d)+1):
+            if d.count(i+1) is 5:
+                print( d.count(i+1))
+                return 50
+        return False
+
+    def sumDie(d):
+        return sum(d)
 
